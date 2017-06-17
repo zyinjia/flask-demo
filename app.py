818f5c8ce3809de64091dd70712749a68e0e7394
Yin-Jia Zhang <zyinjia@gmail.com>
2017-06-17 10:52:12 -0400

    final

--- app.py
+++ app.py
@@ -21,16 +21,15 @@ def index():
         from api_data import get_data
 
         df = get_data(app.vars['name'])
-        p = figure(title='%s Data from Quandle WIKI set' %app.vars['name'] ,
+        p = figure(title='Data from Quandle WIKI set',
                    x_axis_label='Date',
                    x_axis_type='datetime',
                    y_axis_label='Open Price ($)',
-                   toolbar_location="below",
                    toolbar_sticky=False)
         p.circle(df['datetime'], df['open'])
         script, div = components(p)
 
-        return render_template('plot.html', script=script, div=div)
+        return render_template('plot.html', script=script, div=div, ticker=app.vars['name'])
 
 
 if __name__ == '__main__':
